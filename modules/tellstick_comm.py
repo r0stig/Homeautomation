3d6f0726b3cd224628240a86e76833eb2a6c08a0
Robert Säll <pr_125@hotmail.com>
2015-08-29 11:59:15 +0200

    try-except if library isn't found..

--- modules/tellstick_comm.py
+++ modules/tellstick_comm.py
@@ -1,5 +1,5 @@
 # Append module path
-import os, sys
+import os, sys, logging, ConfigParser
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../modules'))
 if not path in sys.path:
     sys.path.insert(1, path)
@@ -8,19 +8,36 @@ del path
 from ctypes import c_int, c_ubyte, c_void_p, POINTER, string_at #imports allowing the use of our library
 import platform, sqlite3, time, dbal, RadioCommunication
 
+config = ConfigParser.ConfigParser()
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../config'))
+config.read(path + '/gcal.cfg')
+
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', filename= config.get('General', 'project_path', 0) + 'logs/receiver_devices.log',level=logging.DEBUG)
 
 class tellstick_comm(RadioCommunication.RadioCommunication):
+	lib_initiated = False
+
 	def __init__(self):
 		self.dbal = dbal.DBAL()
 		# 
 		if (platform.system() == 'Windows'):
 			#Windows
 			from ctypes import windll, WINFUNCTYPE
-			self.lib = windll.LoadLibrary('TelldusCore.dll') #import our library
+			try:
+				self.lib = windll.LoadLibrary('TelldusCore.dll') #import our library
+				lib_initiated = True
+			except OSError:
+				logging.info('Error when loading telldus library')
 		else:
 			#Linux
 			from ctypes import cdll, CFUNCTYPE
-			self.lib = cdll.LoadLibrary('libtelldus-core.so.2') #import our library
+			try:
+				self.lib = cdll.LoadLibrary('libtelldus-core.so.2') #import our library
+				lib_initiated = True
+			except OSError:
+				logging.info('Error when loading telldus library')
+
 		
 	def turnOn(self, id):
 		print "Turning on", id
