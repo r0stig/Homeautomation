c108090bbb116f608d7ac4eab8fa92ee43903a6d
Robert Säll <pr_125@hotmail.com>
2015-10-17 14:13:50 +0200

    Dispatcher for recurring events

---
+++ services/recurring_events.py
@@ -0,0 +1,22 @@
+import os, sys
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../modules'))
+if not path in sys.path:
+    sys.path.insert(1, path)
+del path
+
+import dbal, SunPosition
+
+
+db = dbal.DBAL()
+events = db.get_recurring_events()
+
+for e in events:
+    print e
+    if e['sunrise'] == 1:
+        print 'is sunrise'
+        sunrise = SunPosition.get_today_sunrise()
+        db.insert_event(e['device_id'], e['type'], e['auto'], sunrise)
+    elif e['sunset'] == 1:
+        print 'is sunset'
+        sunset = SunPosition.get_today_sunset()
+        db.insert_event(e['device_id'], e['type'], e['auto'], sunset)
